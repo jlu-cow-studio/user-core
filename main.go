8443261cc329dae9d9ade77f9a9a818549636a52
waynelee-lwc <1418551940@qq.com>
2023-03-22 18:24:45 +0800

    server serving

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/jlu-cow-studio/common/dal/redis"
 	"github.com/jlu-cow-studio/common/dal/rpc/user_core"
 	"github.com/jlu-cow-studio/common/discovery"
@@ -8,10 +13,23 @@ import (
 	"google.golang.org/grpc"
 )
 
+var (
+	port = flag.Int("port", 8080, "The server port")
+)
+
 func main() {
 	discovery.Init()
 	redis.Init()
 
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 	s := grpc.NewServer()
 	user_core.RegisterUserCoreServiceServer(s, &handler.Handler{})
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
