cb32d8ad38618506834b559ecde11e274e85e5da
waynelee-lwc <1418551940@qq.com>
2023-04-01 17:52:29 +0800

    follow count handler

--- handler/UserFollowCountHandler.go
+++ handler/UserFollowCountHandler.go
@@ -3,10 +3,28 @@ package handler
 import (
 	"context"
 
+	"github.com/jlu-cow-studio/common/dal/rpc/base"
 	"github.com/jlu-cow-studio/common/dal/rpc/user_core"
+	"github.com/jlu-cow-studio/user-core/biz"
 )
 
 func (h *Handler) FollowCount(ctx context.Context, req *user_core.FollowCountReq) (res *user_core.FollowCountRes, err error) {
+
+	res = &user_core.FollowCountRes{
+		Base: &base.BaseRes{
+			Message: "",
+			Code:    "498",
+		},
+	}
+
+	if followerCount, followingCount, err := biz.FollowCount(req.UserId); err != nil {
+		res.Base.Message = err.Error()
+		res.Base.Code = "400"
+		return res, nil
+	} else {
+		res.FollowerCount = int32(followerCount)
+		res.FollowingCount = int32(followingCount)
+	}
 
 	return res, nil
 }
